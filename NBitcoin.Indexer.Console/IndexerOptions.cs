ae96196c5ead2900bc73098d35b77e040717e4b5
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-07-13 18:36:26 +0200

    Add console app

---
+++ NBitcoin.Indexer.Console/IndexerOptions.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBitcoin.Indexer.Console
+{
+	class IndexerOptions
+	{
+
+		string _Usage;
+		[HelpOption('?', "help", HelpText = "Display this help screen.")]
+		public string GetUsage()
+		{
+			if(_Usage == null)
+				_Usage = HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+			return _Usage;
+			//
+		}
+	}
+}
---
+++ NBitcoin.Indexer.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBitcoin.Indexer.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var options = new IndexerOptions();
+			if(Parser.Default.ParseArguments(args, options))
+			{
+			}
+		}
+	}
+}
