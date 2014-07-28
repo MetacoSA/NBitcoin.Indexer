8611c64356ad6bb574188bd705beb09cb10277a1
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-07-28 15:56:08 +0200

    nuget

--- NBitcoin.Indexer/Properties/AssemblyInfo.cs
+++ NBitcoin.Indexer/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NBitcoin.Indexer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Nicolas Dorier")]
 [assembly: AssemblyProduct("NBitcoin.Indexer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © AO-IS 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
