f071d34da98c2ea2d4c92bcb23994c13bdd4b188
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-10-13 03:56:04 +0200

    missing file

---
+++ NBitcoin.Indexer/IndexerTransactionRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBitcoin.Indexer
+{
+    public class IndexerTransactionRepository : ITransactionRepository
+    {
+        private readonly IndexerServerConfiguration _Configuration;
+        public IndexerServerConfiguration Configuration
+        {
+            get
+            {
+                return _Configuration;
+            }
+        }
+        public IndexerTransactionRepository(IndexerServerConfiguration config)
+        {
+            if (config == null)
+                throw new ArgumentNullException("config");
+            _Configuration = config;
+        }
+        #region ITransactionRepository Members
+
+        public Transaction Get(uint256 txId)
+        {
+            var tx = _Configuration.CreateIndexerClient().GetTransaction(txId);
+            if (tx == null)
+                return null;
+            return tx.Transaction;
+        }
+
+        public void Put(uint256 txId, Transaction tx)
+        {
+            _Configuration.CreateIndexer().Index(new TransactionEntry.Entity(tx));
+        }
+
+        #endregion
+    }
+}
