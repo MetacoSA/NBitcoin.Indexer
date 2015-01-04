7c4bd13b09a0bf7a454249bc3ad8e2508f0c85b9
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-01-04 02:43:55 +0100

    helper method

--- NBitcoin.Indexer/IndexerConfiguration.cs
+++ NBitcoin.Indexer/IndexerConfiguration.cs
@@ -14,14 +14,14 @@ using System.Threading.Tasks;
 
 namespace NBitcoin.Indexer
 {
-	public class IndexerConfiguration
-	{
-		public static IndexerConfiguration FromConfiguration()
-		{
-			IndexerConfiguration config = new IndexerConfiguration();
-			Fill(config);
-			return config;
-		}
+    public class IndexerConfiguration
+    {
+        public static IndexerConfiguration FromConfiguration()
+        {
+            IndexerConfiguration config = new IndexerConfiguration();
+            Fill(config);
+            return config;
+        }
 
         private JsonSerializerSettings _SerializerSettings = new JsonSerializerSettings();
         public JsonSerializerSettings SerializerSettings
@@ -50,42 +50,42 @@ namespace NBitcoin.Indexer
             GetBlocksContainer().CreateIfNotExists();
         }
 
-		protected static void Fill(IndexerConfiguration config)
-		{
-			var account = GetValue("Azure.AccountName", true);
-			var key = GetValue("Azure.Key", true);
-			config.StorageCredentials = new StorageCredentials(account, key);
-			config.StorageNamespace = GetValue("StorageNamespace", false);
-			var network = GetValue("Bitcoin.Network", false) ?? "Main";
-			config.Network = network.Equals("main", StringComparison.OrdinalIgnoreCase) ?
-									Network.Main :
-							 network.Equals("test", StringComparison.OrdinalIgnoreCase) ?
-							 Network.TestNet : null;
-			if(config.Network == null)
-				throw new ConfigurationErrorsException("Invalid value " + network + " in appsettings (expecting Main or Test)");
+        protected static void Fill(IndexerConfiguration config)
+        {
+            var account = GetValue("Azure.AccountName", true);
+            var key = GetValue("Azure.Key", true);
+            config.StorageCredentials = new StorageCredentials(account, key);
+            config.StorageNamespace = GetValue("StorageNamespace", false);
+            var network = GetValue("Bitcoin.Network", false) ?? "Main";
+            config.Network = network.Equals("main", StringComparison.OrdinalIgnoreCase) ?
+                                    Network.Main :
+                             network.Equals("test", StringComparison.OrdinalIgnoreCase) ?
+                             Network.TestNet : null;
+            if (config.Network == null)
+                throw new ConfigurationErrorsException("Invalid value " + network + " in appsettings (expecting Main or Test)");
 
             config.MainDirectory = GetValue("MainDirectory", false);
             config.Node = GetValue("Node", false);
-		}
+        }
 
-		protected static string GetValue(string config, bool required)
-		{
-			var result = ConfigurationManager.AppSettings[config];
-			result = String.IsNullOrWhiteSpace(result) ? null : result;
-			if(result == null && required)
-				throw new ConfigurationErrorsException("AppSetting " + config + " not found");
-			return result;
-		}
-		public IndexerConfiguration()
-		{
-			Network = Network.Main;
+        protected static string GetValue(string config, bool required)
+        {
+            var result = ConfigurationManager.AppSettings[config];
+            result = String.IsNullOrWhiteSpace(result) ? null : result;
+            if (result == null && required)
+                throw new ConfigurationErrorsException("AppSetting " + config + " not found");
+            return result;
+        }
+        public IndexerConfiguration()
+        {
+            Network = Network.Main;
             Helper.InitializeSerializer(SerializerSettings);
-		}
-		public Network Network
-		{
-			get;
-			set;
-		}
+        }
+        public Network Network
+        {
+            get;
+            set;
+        }
 
         public AzureIndexer CreateIndexer()
         {
@@ -120,30 +120,30 @@ namespace NBitcoin.Indexer
             return fileName;
         }
 
-		string _Container = "indexer";
-		string _TransactionTable = "transactions";
-		string _BalanceTable = "balances";
-		string _ChainTable = "chain";
+        string _Container = "indexer";
+        string _TransactionTable = "transactions";
+        string _BalanceTable = "balances";
+        string _ChainTable = "chain";
         string _WalletTable = "wallets";
         string _WalletBalanceTable = "walletbalances";
 
-		public StorageCredentials StorageCredentials
-		{
-			get;
-			set;
-		}
-		public CloudBlobClient CreateBlobClient()
-		{
-			return new CloudBlobClient(MakeUri("blob"), StorageCredentials);
-		}
-		public IndexerClient CreateIndexerClient()
-		{
-			return new IndexerClient(this);
-		}
-		public CloudTable GetTransactionTable()
-		{
-			return CreateTableClient().GetTableReference(GetFullName(_TransactionTable));
-		}
+        public StorageCredentials StorageCredentials
+        {
+            get;
+            set;
+        }
+        public CloudBlobClient CreateBlobClient()
+        {
+            return new CloudBlobClient(MakeUri("blob"), StorageCredentials);
+        }
+        public IndexerClient CreateIndexerClient()
+        {
+            return new IndexerClient(this);
+        }
+        public CloudTable GetTransactionTable()
+        {
+            return CreateTableClient().GetTableReference(GetFullName(_TransactionTable));
+        }
         public CloudTable GetWalletRulesTable()
         {
             return CreateTableClient().GetTableReference(GetFullName(_WalletTable));
@@ -153,49 +153,53 @@ namespace NBitcoin.Indexer
             return CreateTableClient().GetTableReference(GetFullName(_WalletBalanceTable));
         }
 
-		private string GetFullName(string storageObjectName)
-		{
-			return (StorageNamespace + storageObjectName).ToLowerInvariant();
-		}
-		public CloudTable GetBalanceTable()
-		{
-			return CreateTableClient().GetTableReference(GetFullName(_BalanceTable));
-		}
-		public CloudTable GetChainTable()
-		{
-			return CreateTableClient().GetTableReference(GetFullName(_ChainTable));
-		}
+        public CloudTable GetTable(string tableName)
+        {
+            return CreateTableClient().GetTableReference(GetFullName(tableName));
+        }
+        private string GetFullName(string storageObjectName)
+        {
+            return (StorageNamespace + storageObjectName).ToLowerInvariant();
+        }
+        public CloudTable GetBalanceTable()
+        {
+            return CreateTableClient().GetTableReference(GetFullName(_BalanceTable));
+        }
+        public CloudTable GetChainTable()
+        {
+            return CreateTableClient().GetTableReference(GetFullName(_ChainTable));
+        }
 
-		public CloudBlobContainer GetBlocksContainer()
-		{
-			return CreateBlobClient().GetContainerReference(GetFullName(_Container));
-		}
+        public CloudBlobContainer GetBlocksContainer()
+        {
+            return CreateBlobClient().GetContainerReference(GetFullName(_Container));
+        }
 
-		private Uri MakeUri(string clientType)
-		{
-			return new Uri(String.Format("http://{0}.{1}.core.windows.net/", StorageCredentials.AccountName, clientType), UriKind.Absolute);
-		}
-
-
-		public CloudTableClient CreateTableClient()
-		{
-			return new CloudTableClient(MakeUri("table"), StorageCredentials);
-		}
+        private Uri MakeUri(string clientType)
+        {
+            return new Uri(String.Format("http://{0}.{1}.core.windows.net/", StorageCredentials.AccountName, clientType), UriKind.Absolute);
+        }
 
 
-		public string StorageNamespace
-		{
-			get;
-			set;
-		}
+        public CloudTableClient CreateTableClient()
+        {
+            return new CloudTableClient(MakeUri("table"), StorageCredentials);
+        }
 
-		public IEnumerable<CloudTable> EnumerateTables()
-		{
-			yield return GetTransactionTable();
-			yield return GetBalanceTable();
-			yield return GetChainTable();
+
+        public string StorageNamespace
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<CloudTable> EnumerateTables()
+        {
+            yield return GetTransactionTable();
+            yield return GetBalanceTable();
+            yield return GetChainTable();
             yield return GetWalletRulesTable();
             yield return GetWalletBalanceTable();
-		}
+        }
     }
 }
