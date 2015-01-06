4879f836bb743d4082a072a3531ff1150cde797d
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-01-06 23:10:28 +0100

    2.0.3.19

--- NBitcoin.Indexer/Properties/AssemblyInfo.cs
+++ NBitcoin.Indexer/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.3.18")]
-[assembly: AssemblyVersion("2.0.3.18")]
-[assembly: AssemblyFileVersion("2.0.3.18")]
+[assembly: AssemblyInformationalVersion("2.0.3.19")]
+[assembly: AssemblyVersion("2.0.3.19")]
+[assembly: AssemblyFileVersion("2.0.3.19")]
