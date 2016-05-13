25942e76addc206c633dffb1fc0747c2100f4e8c
Nicolas DORIER <nicolas.dorier@gmail.com>
2016-05-13 12:45:36 +0900

    fix gitlink

--- NBitcoin.Indexer/PushNuget.ps1
+++ NBitcoin.Indexer/PushNuget.ps1
@@ -1,5 +1,6 @@
 impvs
 del *.nupkg 
+msbuild.exe NBitcoin.Indexer.csproj /p:Configuration=Release
 .\GitLink.exe ".." -ignore "build,nbitcoin.indexer.console,nbitcoin.indexer.tests"
 nuGet pack NBitcoin.Indexer.csproj -Build -Properties Configuration=Release -includereferencedprojects
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
