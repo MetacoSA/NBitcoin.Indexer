98d189070edb9ba22982934a593bee2d79f482cd
NicolasDorier <nicolas.dorier@gmail.com>
2016-08-18 15:48:45 +0900

    fix build script

--- NBitcoin.Indexer/PushNuget.ps1
+++ NBitcoin.Indexer/PushNuget.ps1
@@ -1,6 +1,6 @@
 del *.nupkg 
 nuGet pack NBitcoin.Indexer.csproj -Build -Properties Configuration=Release -Properties GitLink=True -includereferencedprojects
-forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE" -source https://api.nuget.org/v3/index.json"
+forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
 (((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
