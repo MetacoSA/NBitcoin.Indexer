28d8953b14a2824d7f4afc432b1cf7b686290c47
NicolasDorier <nicolas.dorier@gmail.com>
2017-01-24 02:18:33 +0900

    Update NBitcoin

--- NBitcoin.Indexer/Properties/AssemblyInfo.cs
+++ NBitcoin.Indexer/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.7.19")]
-[assembly: AssemblyVersion("2.0.7.19")]
-[assembly: AssemblyFileVersion("2.0.7.19")]
+[assembly: AssemblyInformationalVersion("2.0.7.20")]
+[assembly: AssemblyVersion("2.0.7.20")]
+[assembly: AssemblyFileVersion("2.0.7.20")]
     
