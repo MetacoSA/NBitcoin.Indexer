8669310a625774d94e71ffd0db97900a78d8c99d
nicolas.dorier <nicolas.dorier@gmail.com>
2019-06-27 21:03:44 +0900

    Make order balance comparer public

---
+++ NBitcoin.Indexer/OrderBalanceChangeComparer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBitcoin.Indexer
+{
+    public class OrderBalanceChangeComparer : IComparer<OrderedBalanceChange>
+    {
+        private static readonly OrderBalanceChangeComparer _Instance = new OrderBalanceChangeComparer();
+        public static OrderBalanceChangeComparer Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+        public int Compare(OrderedBalanceChange a, OrderedBalanceChange b)
+        {
+            var txIdCompare = a.TransactionId < b.TransactionId ? -1 :
+                              a.TransactionId > b.TransactionId ? 1 : 0;
+            var seenCompare = (a.SeenUtc < b.SeenUtc ? -1 :
+                            a.SeenUtc > b.SeenUtc ? 1 : txIdCompare);
+            if (a.BlockId != null && a.Height is int ah)
+            {
+                // Both confirmed, tie on height then firstSeen
+                if (b.BlockId != null && b.Height is int bh)
+                {
+                    var heightCompare = (ah < bh ? -1 :
+                           ah > bh ? 1 : txIdCompare);
+                    return ah == bh ?
+                           // same height? use firstSeen on firstSeen
+                           seenCompare :
+                           // else tie on the height
+                           heightCompare;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else if (b.BlockId != null && b.Height is int bh)
+            {
+                return 1;
+            }
+            // Both unconfirmed, tie on firstSeen
+            else
+            {
+                return seenCompare;
+            }
+        }
+    }
+}
